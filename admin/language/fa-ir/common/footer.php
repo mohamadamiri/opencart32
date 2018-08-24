fbcc64bd8ee1d4bcbdf08e4b8f1249b25f3bcc12
mohamadamiri <mamadamir.1386@gmail.com>
2018-08-24 18:30:24 +0430

    admin header and footer menu

--- admin/language/fa-ir/common/footer.php
+++ admin/language/fa-ir/common/footer.php
@@ -1,5 +1,5 @@
 <?php
 // Text
-$_['text_footer']  = 'تمامی حقوق برای <a target="_blank" href="http://www.opencartfarsi.com" title="OpenCartFarsi.Com">اپن کارت فارسی دات کام</a> محفوظ است. &copy; 2009-' . date('Y') . ' ';
-$_['text_version'] = 'نسخه %s';
+$_['text_footer']  = 'تمامی حقوق برای شرکت محفوظ است. &copy; 2009-' . date('Y') . ' ';
+$_['text_version'] = '';
 ?>
